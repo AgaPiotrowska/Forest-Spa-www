4f8dbda98ff83efbdcabda3ceff1ffebae8e9a10
AgaPiotrowska <agniegrabo@gmail.com>
2020-11-16 17:04:00 +0100

    social media icons src adjusted

--- src/components/SocialMediaIcons.js
+++ src/components/SocialMediaIcons.js
@@ -4,9 +4,9 @@ const SocialMediaIcons = () => {
     return (
         <div className="icons">
                 <div className="icon"><a href="https://www.facebook.com/forestspakosmetologia"><img
-                    src="images/facebook.svg" alt="FB" /></a></div>
+                    src="/images/facebook.svg" alt="FB" /></a></div>
                 <div className="icon"><a href="https://www.instagram.com/forestspa_kosmetologia_masaz/"><img
-                    src="images/instagram.svg" alt="IG" /></a></div>
+                    src="/images/instagram.svg" alt="IG" /></a></div>
         </div>
     )};
 
